64cc5b50f080119487c1e8717ca2463e0006eb3a
James Garner <james.garner@canonical.com>
2025-03-20 17:34:51 +1300

    docs: links in package docstring

--- src/charmlibs/pathops/__init__.py
+++ src/charmlibs/pathops/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""":mod:`Pathlib`-like interface for local and :class:`ops.Container` filesystem paths."""
+""":mod:`pathlib`-like interface for local and :class:`ops.Container` filesystem paths."""
 
 from __future__ import annotations
 
