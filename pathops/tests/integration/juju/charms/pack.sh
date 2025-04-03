e3f96b609189441d3b89f32a16a98ea9df429532
James Garner <james.garner@canonical.com>
2025-04-04 12:22:24 +1300

    tests: drop deduplication of test charms pyproject.toml

--- pathops/tests/integration/juju/charms/pack.sh
+++ pathops/tests/integration/juju/charms/pack.sh
@@ -15,7 +15,6 @@ rm -rf "$TMPDIR"
 cp --recursive "$CHARMDIR" "$TMPDIR"
 unlink "$TMPDIR/src/common.py"  # remove symlink
 cp common/src/common.py "$TMPDIR/src/"
-cp common/pyproject.toml "$TMPDIR/"
 cat common/actions.yaml >> "$TMPDIR/charmcraft.yaml"
 mkdir "$TMPDIR/pathops"
 cp -r ../../../../pyproject.toml "$TMPDIR/pathops/"
