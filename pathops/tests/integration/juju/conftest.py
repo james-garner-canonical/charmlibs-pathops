2a06c032ff39e264a851bef6dcc5702ce30901ad
James Garner <james.garner@canonical.com>
2025-04-04 15:02:15 +1300

    tests: revert workload to ubuntu as hello-world sometimes fails
    This reverts commit b05bdffcf4340ef7fd74e6a2cafd05a90ad5a246.

--- pathops/tests/integration/juju/conftest.py
+++ pathops/tests/integration/juju/conftest.py
@@ -66,7 +66,7 @@ def _deploy(juju: jubilant.Juju, substrate: str) -> None:
     if substrate == 'kubernetes':
         juju.deploy(
             _get_packed_charm_path(substrate),
-            resources={'workload': 'hello-world'},
+            resources={'workload': 'ubuntu:latest'},
         )
     elif substrate == 'machine':
         juju.deploy(_get_packed_charm_path(substrate))
