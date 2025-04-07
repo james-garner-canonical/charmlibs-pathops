b604b207192a8196c32639f5fc801ed9c2819611
James Garner <james.garner@canonical.com>
2025-04-07 12:59:11 +1200

    style: use single quotes in charmlibs package

--- _charmlibs/src/charmlibs/__init__.py
+++ _charmlibs/src/charmlibs/__init__.py
@@ -5,6 +5,6 @@ For more information, see the PyPI page at https://pypi.org/project/charmlibs
 
 import warnings
 
-__version__ = "0.0.0a0"
+__version__ = '0.0.0a0'
 
-warnings.warn("This package should not be installed.", stacklevel=1)
+warnings.warn('This package should not be installed.', stacklevel=1)
