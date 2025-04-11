2b1fbf2dd9bd980b2e3b431bd1a8ab7dc7495b97
James Garner <james.garner@canonical.com>
2025-04-11 15:13:14 +1200

    tests: correctly call owner and group

--- pathops/tests/integration/juju/charms/kubernetes/src/common.py
+++ pathops/tests/integration/juju/charms/kubernetes/src/common.py
@@ -84,7 +84,7 @@ class Charm(ops.CharmBase):
         except Exception as e:
             event.fail(f'Exception: {e!r}')
             return
-        results = {'user': path.owner, 'group': path.group}
+        results = {'user': path.owner(), 'group': path.group()}
         logger.error(results)
         self.remove_path(path)
         event.set_results(results)
